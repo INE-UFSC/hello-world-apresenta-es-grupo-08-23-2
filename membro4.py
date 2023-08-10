65f73a4d7b92ab010337fcc7da12cc6794aadb3d
Caio <caiocr610@gmail.com>
2023-08-10 15:24:40 -0300

    Adicionando a apresentação do membro 4 (Caio).

--- membro4.py
+++ membro4.py
@@ -1,1 +1,3 @@
 
+def hello():
+	return "Olá, eu sou o Caio César, tenho 19 anos de idade, gosto muito de jogar jogos online, nunca tinha programado antes da graduação e espero aprender e fazer bom proveito da disciplina durante o semestre."
