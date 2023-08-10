e7ab8310b604428c8379a2246a4a658ed8d21177
Caio <caiocr610@gmail.com>
2023-08-10 15:35:21 -0300

    Adicionando membro 4 (Caio).

--- membro4.py
+++ membro4.py
@@ -1,3 +1,3 @@
 
 def hello():
-	return "Olá, eu sou o Caio César, tenho 19 anos de idade, gosto muito de jogar jogos online, nunca tinha programado antes da graduação e espero aprender e fazer bom proveito da disciplina durante o semestre."
+	return "Olá, eu sou o Caio César. Tenho 19 anos de idade. Gosto muito de jogar jogos online. Nunca tinha programado antes da graduação e espero aprender e fazer bom proveito da disciplina durante o semestre."
