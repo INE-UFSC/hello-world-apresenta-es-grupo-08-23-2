baea18559aedb71d12b890080a86eebe785cc2c8
Eduardo Achar <eduardoachar1717@gmail.com>
2023-08-10 15:27:08 -0300

    Registrando membro 3 (Eduardo)

--- membro3.py
+++ membro3.py
@@ -1,2 +1,2 @@
 def hello():
-	return "Olá, eu sou o membro3" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	return "Olá, eu sou o Eduardo Achar, tenho 18 anos, gosto de ir a academia e jogar bola, tenho pouca experiência com programação e pretendo adquirir um maior conhecimento em programação"
