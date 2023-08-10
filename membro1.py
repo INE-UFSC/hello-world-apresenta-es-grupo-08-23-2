dec8a4e20d0d99cbc7322102fcdce1bb7930725e
erika393 <erikaamericano393@gmail.com>
2023-08-10 15:10:11 -0300

    add descricao: membro 01

--- membro1.py
+++ membro1.py
@@ -1,2 +1,2 @@
 def hello():
-	return "Olá, eu sou o membro1" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	return "Olá, eu sou a Erika, tenho 20 anos, gosto de ir à praia, jogar, assistir Masterchef, etc. Tenho 2 anos de experiência em programação, e espero dessa disciplina saber mais sobre boas práticas de programação." #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
